a644a629298b5101ed76075c61b7e3db81169130
Mayra <mayrageovanna602@gmail.com>
2023-02-24 19:10:30 -0300

    Sistema ministrante

--- controller/admin/ministranteController.js
+++ controller/admin/ministranteController.js
@@ -1,16 +1,58 @@
+const { Op } = require('sequelize');
+const models = require('../../database/models')
+const Ministrante = models.Ministrante
+
 //função que lista todos ítens
-async function lst(req, res) {}
+async function lst(req, res) {
+    const ministrante = await Ministrante.findAll()
+    res.render("admin/ministrante/lst", {Ministrantes:ministrantes});
+}
 //função que lista todos ítens de acordo com pesquisa
-async function filtro(req, res) {}
+async function filtro(req, res) {
+    const ministrantes = await Ministrantes.findAll({
+        where:{
+            nome: {
+                [Op.iLike]: '%'+req.body.pesquisar+'%'
+            }
+        }
+    })
+    res.render("admin/ministrante/lst", {Ministrantes:ministrantes});
+}
 //função que abre a tela de add
-async function abreadd(req, res) {}
+async function abreadd(req, res) {
+    res.render("admin/ministrante/add");
+}
 //função que adiciona
-async function add(req, res) {}
+async function add(req, res) {
+    const miinistrante = await Ministrante.create({
+        nome: req.body.nome,
+        email: req.body.email,
+        formacao: req.body.formacao,
+        fotoperfil: req.files.fotoperfil[0].filename,
+    })
+    res.redirect('/admin/ministrante/lst');
+}
 //função que abre tela de edt
-async function abreedt(req, res) {}
+async function abreedt(req, res) {
+    const ministrante = await Ministrante.findByPk(req.params.id);
+    res.render("admin/ministrante/edt", {Ministrante:ministrante});
+}
 //função que edita
-async function edt(req, res) {}
+async function edt(req, res) {
+    const ministrante = await Ministrante.findByPk(req.params.id);
+    await ministrante.update({
+        nome: req.body.nome,
+        email: req.body.email,
+        formacao: req.body.formacao,
+        fotoperfil: req.files['fotoperfil'].filename
+    })
+    res.redirect('/admin/ministrante/lst')
+}
 //função que deleta ítens
-async function del(req, res) {}
+async function del(req, res) {
+    const ministrante = await Ministrante.findByPk(req.params.id);
+    await ministrante.destroy()
+    res.redirect('/admin/ministrante/lst')
+}
 
 module.exports = { lst, filtro, abreadd, add, abreedt, edt, del };
--- routes/admin/ministranteRoute.js
+++ routes/admin/ministranteRoute.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const ministranteController = require("../../controller/admin/ministranteController");
+const upload = require("../../config/upload");
 
 //rota para listar todos os ministrantes
 router.get("/ministrante/lst", ministranteController.lst);
@@ -9,12 +10,18 @@ router.post("/ministrante/lst", ministranteController.filtro);
 //rota para abrir a tela de adicionar ministrante
 router.get("/ministrante/add", ministranteController.abreadd);
 //rota que adiciona o ministrante
-router.post("/ministrante/add", ministranteController.add);
+router.post("/ministrante/add", 
+    upload.fields([
+        {name: 'fotoperfil', maxCount:1}
+    ]), ministranteController.add);
 //rota para abrir a tela de editar ministrante
-router.get("/ministrante/edt", ministranteController.abreedt);
+router.get("/ministrante/edt/:id", ministranteController.abreedt);
 //rota para editar o ministrante
-router.post("/ministrante/edt", ministranteController.edt);
+router.post("/ministrante/edt/:id",
+    upload.fields([
+        {name: 'fotoperfil', maxCount:1},
+    ]), ministranteController.edt);
 //rota para deletar ministrante
-router.get("/ministrante/del", ministranteController.del);
+router.get("/ministrante/del/:id", ministranteController.del);
 
 module.exports = router;
